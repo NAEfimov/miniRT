34d8250b52497c18f2493946c60ff6184cff600c
nefimov <n.a.efimov@gmail.com>
2025-11-15 13:59:26 +0100

    Add MLX code to main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,52 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 256
+#define HEIGHT 256
 
-int main(void)
+// Exit the program as failure.
+static void ft_error(void)
 {
-    printf("Hello, World!\n");
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
 
-    return (0);
+// Print the window width and height.
+static void ft_hook(void* param)
+{
+	const mlx_t* mlx = param;
+
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+	usleep(20000);
+}
+
+int	main(void)
+{
+
+	// MLX allows you to define its core behaviour before startup.
+	mlx_set_setting(MLX_MAXIMIZED, false);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "miniRT", false);
+	if (!mlx)
+		ft_error();
+
+	/* Do stuff */
+
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	memset(img->pixels, 255, img->width * img->height * sizeof(int32_t));
+
+	// mlx_put_pixel(img, WIDTH / 2, HEIGHT / 2, 0xFF0000FF);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (0);
 }
