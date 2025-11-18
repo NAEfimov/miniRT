54967b0052fca8ed4bc029180418aa1a5e205a6b
nefimov <nefimov@student.42berlin.de>
2025-11-18 18:18:18 +0100

    finish clean_scene() function and add ft_lstclear.c to libft Makefile

--- src/scene_clean.c
+++ src/scene_clean.c
@@ -1,10 +1,24 @@
 #include <stdlib.h>
+#include "libft.h"
 #include "main.h"
 
 int	clean_scene(t_scene *scene)
 {
 	if (scene->image)
 		free(scene->image);
+	if (scene->camera)
+		free(scene->camera);
+	if (scene->a_light)
+		free(scene->a_light);
+	if (scene->light)
+		free(scene->light);
+	
+	if (scene->sphere)
+		ft_lstclear(&(scene->sphere), free);
+	if (scene->cyl)
+		ft_lstclear(&(scene->cyl), free);
+	if (scene->plane)
+		ft_lstclear(&(scene->plane), free);
 
 	return (0);
 }
