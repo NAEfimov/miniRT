bea7627ce8c8ad7b02481a3aa6948992f4ff25d3
nefimov <nefimov@student.42berlin.de>
2025-11-20 21:16:26 +0100

    add read_uint8.c file and function

--- include/parser/read.h
+++ include/parser/read.h
@@ -3,10 +3,11 @@
 
 # include "main.h"
 
-int	read_int(int *num, char *word);
-int	read_double(double *num, char *word);
-int	read_color(t_scene *scene, t_color *color, char *word);
-int	read_normal(t_scene *scene, t_vec *norm, char *word);
-int	read_vec(t_scene *scene, t_vec *vec, char *word);
+int read_int(int *num, char *word);
+int	read_uint8(uint8_t *num, char *word);
+int read_double(double *num, char *word);
+int read_color(t_color *color, char *word);
+int read_normal(t_scene *scene, t_vec *norm, char *word);
+int read_vec(t_scene *scene, t_vec *vec, char *word);
 
 #endif
