985f4b585a2a3ba43a19358faa822b5aa2c2d697
plantsov <plantsov@student.42berlin.de>
2025-11-21 17:35:59 +0100

    add vector maths

---
+++ include/vector.h
@@ -0,0 +1,6 @@
+#include "main.h"
+
+t_vec  vec_add(t_vec a, t_vec b);
+t_vec  vec_scl(t_vec a, double s);
+t_vec  vec_nrm(t_vec v);
+t_vec  vec_crs(t_vec a, t_vec b);
---
+++ src/vector.c
@@ -0,0 +1,73 @@
+#include "../include/vector.h"
+
+/**
+ * Adds two vectors component-wise.
+ * @param a First vector
+ * @param b Second vector
+ * 
+ * @return Resulting vector (a + b)
+ */
+t_vec	vec_add(t_vec a, t_vec b)
+{
+	t_vec	result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+	result.z = a.z + b.z;
+	return (result);
+}
+
+/**
+ * Multiplies a vector by a scalar.
+ * @param a Vector
+ * @param s Scalar
+ * 
+ * @return Resulting vector (a * s)
+ */
+t_vec	vec_scl(t_vec a, double s)
+{
+	t_vec	result;
+
+	result.x = a.x * s;
+	result.y = a.y * s;
+	result.z = a.z * s;
+	return (result);
+}
+
+/**
+ * Normalizes a vector to length 1.
+ * @param v Vector
+ * 
+ * @return Resulting normalized vector or original (if length is 0)
+ */
+t_vec	vec_nrm(t_vec v)
+{
+	double	len;
+	t_vec	result;
+
+	len = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+	if (len == 0)
+		return (v);
+	result.x = v.x / len;
+	result.y = v.y / len;
+	result.z = v.z / len;
+	return (result);
+}
+
+/**
+ * Computes the cross product of two vectors.
+ * 
+ * @param a First vector
+ * @param b Second vector
+ * 
+ * @return Resulting vector (a * b)
+ */
+t_vec	vec_crs(t_vec a, t_vec b)
+{
+	t_vec	result;
+
+	result.x = a.y * b.z - a.z * b.y;
+	result.y = a.z * b.x - a.x * b.z;
+	result.z = a.x * b.y - a.y * b.x;
+	return (result);
+}
