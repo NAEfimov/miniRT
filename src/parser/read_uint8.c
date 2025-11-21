bd49c7772a2eb6e70595ff989cd8c042abf7c74f
nefimov <nefimov@student.42berlin.de>
2025-11-21 10:52:55 +0100

    optimise read_uint8() func

--- src/parser/read_uint8.c
+++ src/parser/read_uint8.c
@@ -2,38 +2,10 @@
 
 int	read_uint8(uint8_t *num, char *word)
 {
-	int sign = 1;
-	char *ptr = word;
-	int result = 0;
-	
-	if (!word || !*word || !num) {
-        return (1);  // Error: null or empty string
-    }
-    // Handle sign
-    if (*ptr == '-') {
-        sign = -1;
-        ptr++;
-    } else if (*ptr == '+') {
-        ptr++;
-    }
-    // Must have at least one digit
-    if (*ptr < '0' || *ptr > '9') {
-        return (1);
-    }
-    // Parse digits
-    while (*ptr >= '0' && *ptr <= '9') {
-        result = result * 10 + (*ptr - '0');
-        ptr++;
-        // Basic overflow check (optional, for int range)
-        if (result < 0) {  // Overflow occurred
-            return (1);
-        }
-    }
-    // Ensure end of string
-    if (*ptr != '\0') {
-        return (1);
-    }
-    result *= sign;
+	int	result;
+
+	if (read_int(&result, word))
+		return (1);
 	if (result < 0 || result > 255)
 		return (1);
 	*num = (uint8_t)result;
